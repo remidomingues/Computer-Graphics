fffd7c42df5a1052043efc2103a944197a7b78dc
Rémi Domingues <remidomingues@live.fr>
2015-04-19 21:44:21 +0200

    Implemented direct light rendering

--- raytracing/raytracing/raytracing/raytracing.cpp
+++ raytracing/raytracing/raytracing/raytracing.cpp
@@ -19,13 +19,15 @@ struct Intersection
 	int triangleIndex;
 };
 
+typedef enum { RIGHT, LEFT, FORWARD, BACKWARD, UP, DOWN } Direction;
+
 // ----------------------------------------------------------------------------
 // GLOBAL VARIABLES
 int time;
 
 /* Screen */
-const int SCREEN_WIDTH = 150;
-const int SCREEN_HEIGHT = 150;
+const int SCREEN_WIDTH = 200;
+const int SCREEN_HEIGHT = 200;
 SDL_Surface* screen;
 
 /* Model */
@@ -42,22 +44,27 @@ mat3 R = mat3(0, 0, 0, 0, 0, 0, 0, 0, 0);
 float yaw = 0;
 // Rotation constant - Angle update on the y axis for a rotation
 float ROTATION = 0.3;
-// Translation
-float TRANSLATION = 1;
+
+/* Light */
+vec3 lightPos(0, -0.5, -0.7);
+vec3 lightColor = 14.f * vec3(1, 1, 1);
 
 // ----------------------------------------------------------------------------
 // FUNCTIONS
 
-void updateRotationMatrix();
+void UpdateRotationMatrix();
 bool ClosestIntersection(vec3 start, vec3 dir, const vector<Triangle>& triangles, Intersection& closestIntersection);
 void Update();
 void Draw();
+vec3 GetAxis(Direction dir);
+float GetDistance(vec3 p1, vec3 p2);
+vec3 DirectLight(const Intersection& i);
 
 int main( int argc, char* argv[] )
 {
 	screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT );
 	R[1][1] = 1;
-	updateRotationMatrix();
+	UpdateRotationMatrix();
 	time = SDL_GetTicks();	// Set start value for timer.
 
 	// Initialize the model
@@ -103,12 +110,33 @@ bool ClosestIntersection(vec3 start, vec3 dir, const vector<Triangle>& triangles
 }
 
 /* Update the rotation matrix */
-void updateRotationMatrix()
+void UpdateRotationMatrix()
 {
 	R[0][0] = cos(yaw);
 	R[0][2] = sin(yaw);
 	R[2][0] = -sin(yaw);
 	R[2][2] = cos(yaw);
+}
+
+vec3 GetAxis(Direction dir) {
+	if (dir == Direction::UP) {
+		return -vec3(R[1][0], R[1][1], R[1][2]);
+	}
+	if (dir == Direction::DOWN) {
+		return vec3(R[1][0], R[1][1], R[1][2]);
+	}
+	if (dir == Direction::RIGHT) {
+		return vec3(R[0][0], R[0][1], -R[0][2]);
+	}
+	if (dir == Direction::LEFT) {
+		return -vec3(R[0][0], R[0][1], -R[0][2]);
+	}
+	if (dir == Direction::FORWARD) {
+		return vec3(-R[2][0], R[2][1], R[2][2]);
+	}
+	if (dir == Direction::BACKWARD) {
+		return -vec3(-R[2][0], R[2][1], R[2][2]);
+	}
 }
 
 void Update()
@@ -119,19 +147,18 @@ void Update()
 	time = t2;
 	cout << "Render time: " << dt << " ms." << endl;
 
-	// Move and rotate camera
 	Uint8* keystate = SDL_GetKeyState(0);
+
+	/* Camera rotation and translation */
 	if (keystate[SDLK_UP])
 	{
 		// Move camera forward
-		vec3 forward(-R[2][0], R[2][1], R[2][2]);
-		cameraPos += TRANSLATION * forward;
+		cameraPos += GetAxis(Direction::FORWARD);
 	}
 	if (keystate[SDLK_DOWN])
 	{
 		// Move camera backward
-		vec3 forward(-R[2][0], R[2][1], R[2][2]);
-		cameraPos += TRANSLATION * -forward;
+		cameraPos += GetAxis(Direction::BACKWARD);
 	}
 	if (keystate[SDLK_LEFT])
 	{
@@ -139,11 +166,10 @@ void Update()
 		// Rotate camera to the right
 		yaw += ROTATION;
 
-		updateRotationMatrix();
+		UpdateRotationMatrix();
 
 		// Move camera to the left
-		vec3 right(R[0][0], R[0][1], -R[0][2]);
-		cameraPos += TRANSLATION * -right;
+		cameraPos += GetAxis(Direction::LEFT);
 	}
 	if (keystate[SDLK_RIGHT])
 	{
@@ -151,12 +177,38 @@ void Update()
 		// Rotate camera to the left
 		yaw -= ROTATION;
 
-		updateRotationMatrix();
+		UpdateRotationMatrix();
 
 		// Move camera to the right
-		vec3 right(R[0][0], R[0][1], -R[0][2]);
-		cameraPos += TRANSLATION * right;
+		cameraPos += GetAxis(Direction::RIGHT);
 	}
+
+	/* Light source translation */
+	if (keystate[SDLK_w])
+	{
+		lightPos += GetAxis(Direction::FORWARD);
+	}
+	if (keystate[SDLK_s])
+	{
+		lightPos += GetAxis(Direction::BACKWARD);
+	}
+	if (keystate[SDLK_a])
+	{
+		lightPos += GetAxis(Direction::LEFT);
+	}
+	if (keystate[SDLK_d])
+	{
+		lightPos += GetAxis(Direction::RIGHT);
+	}
+	if (keystate[SDLK_q])
+	{
+		lightPos += GetAxis(Direction::UP);
+	}
+	if (keystate[SDLK_e])
+	{
+		lightPos += GetAxis(Direction::DOWN);
+	}
+		
 }
 
 void Draw()
@@ -179,7 +231,8 @@ void Draw()
 			}
 			// Fill a pixel with the color of the closest triangle intersecting the ray, black otherwise
 			if (ClosestIntersection(cameraPos, d_ray, triangles, closestIntersection)) {
-				PutPixelSDL(screen, x, y, triangles[closestIntersection.triangleIndex].color);
+				PutPixelSDL(screen, x, y, DirectLight(closestIntersection));
+				// triangles[closestIntersection.triangleIndex].color
 			}
 			else {
 				PutPixelSDL(screen, x, y, black);
@@ -192,4 +245,23 @@ void Draw()
 		SDL_UnlockSurface(screen);
 
 	SDL_UpdateRect( screen, 0, 0, 0, 0 );
+}
+
+float GetDistance(vec3 p1, vec3 p2) {
+	float dx = p2.x - p1.x;
+	float dy = p2.y - p1.y;
+	float dz = p2.z - p1.z;
+	return sqrt(dx * dx + dy * dy + dz * dz);
+}
+
+/* Return the light intensity hitting the given intersection from the light source */
+vec3 DirectLight(const Intersection& i) {
+	float dist = GetDistance(i.position, lightPos);
+	float div = 4 * M_PI * dist * dist;
+	float max = fmax(glm::dot(lightPos - i.position, triangles[i.triangleIndex].normal), 0);
+	vec3 power = lightColor;	
+	power.x = power.x * max / div;
+	power.y = power.y * max / div;
+	power.z = power.z * max / div;
+	return power;
 }
